54949dcf6b86323d573cd3a44430a815100e5c88
Andrey <itsfera85@gmail.com>
2024-03-14 04:25:44 +0500

    ДОбавил комментарии

--- control_1/Program.cs
+++ control_1/Program.cs
@@ -3,37 +3,37 @@
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
 
-string [] CreateArray(int size)
+string [] CreateArray(int size)   // функция создания исходного массива
 {  
     string [] array = new string[size];
     Console.WriteLine("Введите строки через Enter");
     for (int i=0; i<array.Length; i++)
     {        
-        array[i] = Console.ReadLine();       
+        array[i] = Console.ReadLine();     // заполняется массив значениями, вводимыми с клавиатуры   
     }
     return array;
 }
 
 Console.WriteLine("Введите количество строк");
 int n = Convert.ToInt32(Console.ReadLine());
-string [] array = CreateArray(n);
-Console.WriteLine($"Исходный массив: [{string.Join("; ", array)}]");
 
-string [] NewArray(string [] original_array)
+string [] original_array = CreateArray(n);       //создали массив
+
+string [] NewArray(string [] original_array)  // функция, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 {
 int count=0;  
    for (int i=0; i<original_array.Length; i++)
    {    
     if (original_array[i].Length<4)
     {
-        count++;
+        count++;                      // определяем размерность нового массива
     }
    }  
 string [] new_array = new string[count];
 int j=0;
 for (int i=0; i<original_array.Length; i++)
    {    
-    if (original_array[i].Length<4)
+    if (original_array[i].Length<4)      // если длина строки исходного массива меньше или равна 3 символам, то добавлем эту строку в новый массив
     {
         new_array[j]=original_array[i];
         j++;
@@ -41,5 +41,8 @@ for (int i=0; i<original_array.Length; i++)
    }
    return new_array;
 }
-string [] new_array =NewArray(array);
+string [] new_array = NewArray(original_array);
+
+Console.WriteLine($"Исходный массив: [{string.Join("; ", original_array)}]");
 Console.WriteLine($"Модифицированный массив: [{string.Join("; ", new_array)}]");
+
